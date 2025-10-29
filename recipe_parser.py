4072dbadc7936d4dc7bf8d5164531c70947f4554
Daeyong Kim <youbinlove607@gmail.com>
2025-10-29 14:15:59 +0900

    fix recipe_parser

--- recipe_parser.py
+++ recipe_parser.py
@@ -1,36 +1,7 @@
 from frontmatter import Frontmatter
 
+a = 100
 
-
-
-
-
-
-
-
-
-a=100
-
-
-
-
-
-
-
-
-
-
-
-
-b=Frontmatter.read_file("야채볶음밥.md")
-
-
-
-
-
-
-
-
-
+b = Frontmatter.read_file("야채볶음밥.md")
 
 print(b["attributes"])
